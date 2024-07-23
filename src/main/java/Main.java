99d6b61f90c7870a64035858d049c41fc8ca63ca
Andrey Pchelintsev <doctor.madbee@gmail.com>
2024-07-23 23:00:05 +0700

    Пофиксены все замечания из предыдущей версии. 1. обработка пустого названия авто. 2. обработка при вводе скорости символов отличных от цифр. 3. вывод правильных результатов в пройденных километрах. 4. учет оставленных комментариев в PR 5. Доработка основного блока, вынос всей математики в классы.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,37 +6,9 @@ public class Main {
     public static void main(String[] args) {
         int carQTY = 3; //количество автомобилей участвующих в гонке.
         Cars cars = new Cars(); // Гараж с автомобилями.
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Добро пожаловать на 24 часа Ле-Мана!\nДавайте зарегистрируем автомобили.");
-
-        while (carQTY > 0) {
-            String name;
-            int speed;
-            while (true) {
-                System.out.format("Вам осталось ввести %d авто.\nВведите имя машины:", carQTY);
-                name = scanner.nextLine();
-                if (!cars.isExist(name)) {
-                    break;
-                }
-                System.out.format("'%s' уже существует, пожалуйста используйте уникальные имена.\n", name);
-            }
-            while (true) {
-                System.out.format("Введите скорость автомобиля '%s'(от 0 до 250км/ч):", name);
-                speed = Integer.parseInt(scanner.nextLine());
-                if (speed <= 250 && speed >= 0) {
-                    break;
-                } else {
-                    System.out.println("Вы ввели не верное значение.");
-                }
-            }
-            cars.addCar(name, speed);
-            carQTY--;
-        }
-        System.out.println("Все автомобили зарегистрированы, начинаем гонку.");
-        Race race = new Race(cars);
-        race.start();
-
+        cars.registration(carQTY); //регистрация машин.
+        Race race = new Race(cars); //регистрация гонки.
+        race.start(); //запуск гонки
     }
 }
 
@@ -57,6 +29,44 @@ class Cars {
         return cars;
     }
 
+    public void registration(int qty) {
+        System.out.println("Добро пожаловать на 24 часа Ле-Мана!\nДавайте зарегистрируем автомобили.");
+        Scanner scanner = new Scanner(System.in);
+        while (qty > 0) {
+
+            String name;
+            String speed;
+            int speedInt;
+
+            while (true) {
+                System.out.format("\nВам осталось ввести %d авто.\nВведите имя машины:", qty);
+                name = scanner.nextLine();
+                if (!this.isExist(name) && !name.trim().isEmpty()) {
+                    break;
+                }
+                if (name.trim().isEmpty()) {
+                    System.out.println("имя не может быть пустым.\n");
+                } else {
+                    System.out.format("'%s' уже существует, пожалуйста используйте уникальные имена.\n", name);
+                }
+            }
+            while (true) {
+                System.out.format("Введите скорость автомобиля '%s'(от 0 до 250км/ч):", name);
+                speed = scanner.nextLine();
+                if (canConvertToInt(speed)) {
+                    speedInt = Integer.parseInt(speed);
+                    if (speedInt >= 0 && speedInt <= 250) {
+                        break;
+                    }
+                }
+                System.out.println("Вы ввели не верное значение.");
+            }
+            this.addCar(name, speedInt);
+            qty--;
+        }
+    }
+
+
     public boolean isExist(String name) {
         for (Car existingCar : cars) {
             if (existingCar.name.equals(name)) {
@@ -68,6 +78,10 @@ class Cars {
 
     public void addCar(String name, int speed) {
         cars.add(new Car(name, speed));
+    }
+
+    boolean canConvertToInt(String str) {
+        return str.matches("-?\\d+");
     }
 
 }
@@ -82,11 +96,12 @@ class Race {
     }
 
     public void start() {
-        for (Car car: cars.getInfo()){
+        System.out.println("Все автомобили зарегистрированы, начинаем гонку.");
+        for (Car car : cars.getInfo()) {
             if (car.speed > leader.speed) {
                 leader = car;
             }
         }
-        System.out.format("Финиш\nПобедил автомобиль '%s'. Он проехал трассу за %d ч.", leader.name, time /leader.speed);
+        System.out.format("Финиш\nПобедил автомобиль '%s'. Он проехал %dкм за 24 часа.", leader.name, time * leader.speed);
     }
 }
